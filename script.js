b78837deb0505b3f1b4323484d86bc839b452e54
lawrencianimako@gmail.com <lawrencianimako@gmail.com>
2020-05-05 17:15:33 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+      function displayMessage() {
+        alert("Your form has ben successfully submitted");
+}
+
+
+function validation() {
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.{2,4}+$/;
+    return emailPattern.test(elementvalue);
+}
+
+     
+
+
