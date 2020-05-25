43784e11c1e1105609f47b0c5f39a16e8a6fb789
Lawrencia Nimako <lawrencianimako@gmail.com>
2020-05-25 18:50:39 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    let img = document.getElementById('img');
+   onclick = function(){
+       img.src = "images/486292.jpg";
+   }
+   onclick = function(){
+       img.src = "images/499787.png";
+    }
+onclick = function(){
+       img.src = "images/513116.png";
+    }
+onclick = function(){
+        img.src = "images/images.jpg";
+     }
+ ​}
+
+​
+​
+    
+​
+​
